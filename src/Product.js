009db53b99931665a2fecf91ce153c561a729e0c
roaa-kittaneh <roaakittaneh2004@gmail.com>
2023-10-24 11:44:29 +0300

    first

---
+++ src/Product.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+   <>
+   <div className='pro'>
+    <div> 
+        <h3>product1</h3>
+    </div>
+
+    <div> 
+        <h3>product1</h3>
+    </div>
+
+    <div> 
+        <h3>product1</h3>
+    </div>
+
+   </div>
+
+   </>
+  );
+}
